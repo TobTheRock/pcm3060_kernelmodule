59f498fa28d364edda90508aa6531601767d1a28
Tobias Waurick <twaurick@gmx.net>
2020-08-19 13:13:57 +0200

    Renamed deivce

--- src/periphery/clock_generator.c
+++ src/periphery/clock_generator.c
@@ -11,13 +11,13 @@
 #define DUTCYCLE_PERCENT 50
 #define DUTCYCLE_PERCENT_SCALE 100
 
-
+struct pwm_device* dev_pwm_sck;
 
 int clock_generator_init(struct device *pdev, const unsigned int frequency)
 {
     int ret = 0,
         period = HZ_TO_NS(frequency);
-    struct pwm_device* pwm_sck1_ptr;
+    struct pwm_device* dev_pwm_sck;
     struct pwm_state new_sck1_state = {
         .period = period,
         .polarity = PWM_POLARITY_NORMAL,
@@ -38,10 +38,10 @@ int clock_generator_init(struct device *pdev, const unsigned int frequency)
     else
     {
         INFO("Requesting PWM for SCK1");
-        pwm_sck1_ptr = devm_pwm_get(pdev, DEVICETREE_PCM3060_SCK1_PWMNAME);
-        if (IS_ERR(pwm_sck1_ptr))
+        dev_pwm_sck = devm_pwm_get(pdev, DEVICETREE_PCM3060_SCK1_PWMNAME);
+        if (IS_ERR(dev_pwm_sck))
         {
-            ERROR("Requesting PWM  for SCK1 failed %p", ERR_CAST(pwm_sck1_ptr));
+            ERROR("Requesting PWM  for SCK1 failed %p", ERR_CAST(dev_pwm_sck));
             ret = -EIO;
         }
     }
@@ -50,7 +50,7 @@ int clock_generator_init(struct device *pdev, const unsigned int frequency)
     {
         INFO("Applying new state for SCK1: period[%dns], duty cycle[%dns], polarity[%d], enable [%d]",
         new_sck1_state.period, new_sck1_state.duty_cycle, new_sck1_state.polarity, new_sck1_state.enabled);
-        ret = pwm_apply_state(pwm_sck1_ptr, &new_sck1_state);
+        ret = pwm_apply_state(dev_pwm_sck, &new_sck1_state);
         INFO("Requested PWM");
     }
 
@@ -60,5 +60,6 @@ int clock_generator_init(struct device *pdev, const unsigned int frequency)
 int clock_generator_cleanup(struct device *pdev)
 {
     DEBUG("");
+    pwm_put(dev_pwm_sck);
     return 0;
 }
